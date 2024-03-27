bb746dbf7c558e4cc94010acda7c72f4d9ea0d95
Lasse Wolpmann <33729366+lassewolpmann@users.noreply.github.com>
2024-03-27 10:11:37 +0200

    Visualization: - drawing maze with pygame - button to regenerate maze

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from random import randint
+import pygame
+
+WIDTH = 1024
+HEIGHT = 1024
+CELL_SIZE = 15
+WALL_COLOR = (0, 0, 0)
+PATH_COLOR = (255, 255, 255)
+START_COLOR = (0, 255, 0)
+END_COLOR = (255, 0, 0)
+PATHFINDING_COLOR = (0, 0, 255)
 
 
 class Maze:
@@ -11,11 +21,10 @@ class Maze:
         self.end_position = (0, 0)
 
         self.create_maze()
-        self.place_walls()
-        self.place_start_and_end_position()
-        self.create_graph()
 
     def create_maze(self):
+        self.maze = []
+
         for row in range(self.rows):
             r = []
             for col in range(self.cols):
@@ -25,6 +34,10 @@ class Maze:
                     r.append("-")
 
             self.maze.append(r)
+
+        self.place_walls()
+        self.place_start_and_end_position()
+        self.create_graph()
 
     def place_walls(self):
         for row in range(0, self.rows):
@@ -97,8 +110,77 @@ class Maze:
         return "\n".join(rows)
 
 
+class Visualizer:
+    def __init__(self, maze_object: Maze):
+        pygame.init()
+        pygame.display.set_caption("DSA Pathfinding Visualizer")
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+        self.maze = maze_object
+
+        self.regen_button = Button("Regenerate Maze", (self.maze.rows + 1) * CELL_SIZE, 1 * CELL_SIZE, self.screen)
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    if self.regen_button.check_collision(pos):
+                        self.maze.create_maze()
+
+                if event.type == pygame.VIDEORESIZE:
+                    self.screen = pygame.display.set_mode((event.w, event.h),
+                                                          pygame.RESIZABLE)
+
+            self.screen.fill(PATH_COLOR)
+            self.draw_maze()
+            self.regen_button.draw()
+            pygame.display.flip()
+
+        pygame.quit()
+
+    def draw_maze(self):
+        for x, row in enumerate(self.maze.maze):
+            for y, cell in enumerate(row):
+                if cell == "#":
+                    color = WALL_COLOR
+                elif cell == "S":
+                    color = START_COLOR
+                elif cell == "E":
+                    color = END_COLOR
+                else:
+                    color = PATH_COLOR
+
+                pygame.draw.rect(self.screen, color, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
+
+class Button:
+    def __init__(self, button_text, x, y, screen):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.width = 200
+        self.height = 50
+        self.color = (0, 200, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont("arial", 18)
+        self.button_text = button_text
+        self.rect = pygame.Rect((WIDTH - self.width) // 2, (HEIGHT - self.height) // 2, self.width,
+                                self.height)
+
+    def draw(self):
+        pygame.draw.rect(self.screen, self.color, (self.x, self.y, self.width, self.height))
+        text = self.font.render(str(self.button_text), True, self.text_color)
+        text_rect = text.get_rect(center=(self.x + self.width // 2, self.y + self.height // 2))
+        self.screen.blit(text, text_rect)
+
+    def check_collision(self, mouse_pos) -> bool:
+        return self.x <= mouse_pos[0] <= self.x + self.width and self.y <= mouse_pos[1] <= self.y + self.height
+
+
 if __name__ == "__main__":
     maze = Maze()
-    print(maze)
-    shortest_path = maze.bfs()
-    print(shortest_path)
+    # shortest_path = maze.bfs()
+    visualizer = Visualizer(maze)
